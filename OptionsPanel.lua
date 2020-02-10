7c5483038e9ed20b8a987dd3731e3c82892344b7
Tercio Jose <terciob19@hotmail.com>
2020-02-09 23:28:45 -0300

    [Options Panel] finished the revamp

--- OptionsPanel.lua
+++ OptionsPanel.lua
@@ -24,6 +24,8 @@ local CONST_MENU_STARTPOS_Y = -30
 local CONST_OPTIONSPANEL_STARTPOS_X = 208
 local CONST_OPTIONSPANEL_STARTPOS_Y = -36
 
+local CONST_MENU_FONT_SIZE = 12
+
 local CACHE_ALL_PLUGINS_INSTALLED = {}
 
 function RA.OpenMainOptions (plugin)
@@ -37,38 +39,15 @@ function RA.OpenMainOptions (plugin)
 		return
 	end
 
-
 	RA.db.options_panel = RA.db.options_panel or {}
-
 
 	local f = RA:CreateStandardFrame (UIParent, CONST_OPTIONS_FRAME_WIDTH, CONST_OPTIONS_FRAME_HEIGHT, "Raid Assist (|cFFFFAA00/raa|r)", "RaidAssistOptionsPanel", RA.db.options_panel)
 	f:SetBackdropBorderColor (1, .7, 0, .8)
 	f:SetBackdropColor (0, 0, 0, 1)
 	DetailsFramework:ApplyStandardBackdrop (f, 0.9)
-	
-	--local label_plugins = RA:CreateLabel (f, "Plugins:")
-	--label_plugins:SetPoint (10, -35)	
 
-	--local label_mods = RA:CreateLabel (f, "Options Panel For:")
-	--label_mods:SetPoint (10, -405)
-	
-	f.Menu = {}
-	f.Mods = {}
-	f.Main = {}
-	f.Menu.x = 10
-	f.Menu.y = -50
-	f.Menu.button_width = 150
-	f.Menu.button_height = 24
-	f.Mods.button_width = 140
-	f.Mods.button_height = 16
-	f.Mods.x = 10
-	f.Mods.y = -420
-	f.Main.x = 190
-	f.Main.y = -40
 	f.AllOptionsButtons = {}
 	f.AllOptionsPanels = {}
-	f.CurrentSelected = nil --not initialized
-	
 	
 	--create the footer and attach it into the bottom of the main frame
 		local statusBar = CreateFrame ("frame", nil, f)
@@ -77,7 +56,7 @@ function RA.OpenMainOptions (plugin)
 		statusBar:SetHeight (20)
 		DetailsFramework:ApplyStandardBackdrop (statusBar)
 		statusBar:SetAlpha (0.8)
-		DetailsFramework:BuildStatusbarAuthorInfo (statusBar)	
+		DetailsFramework:BuildStatusbarAuthorInfo (statusBar)
 	
 
 	--keybind to open the panel
@@ -86,11 +65,9 @@ function RA.OpenMainOptions (plugin)
 
 	--create the left menu
 		--when the player select a plugin in the plugin scroll
-		local on_select_plugin =  function (button, mouse, plugin)
+		local onSelectPlugin =  function (button, mouse, plugin)
 			--reset
 			f:ResetButtonsAndPanels()
-			--set the current panel
-			f.CurrentSelected = plugin
 			
 			--make the button change its text color
 			plugin.textcolor = "orange"
@@ -104,7 +81,7 @@ function RA.OpenMainOptions (plugin)
 	
 		--change the selected plugin from inside plugins
 		RA.OpenMainOptionsForPlugin = function (plugin)
-			return on_select_plugin (nil, nil, plugin)
+			return onSelectPlugin (nil, nil, plugin)
 		end
 
 
@@ -156,7 +133,7 @@ function RA.OpenMainOptions (plugin)
 					local button = self:GetLine(i)
 					button:SetText (text)
 
-					button:SetClickFunction(on_select_plugin, pluginObject)
+					button:SetClickFunction(onSelectPlugin, pluginObject)
 
 					if (iconTexcoord) then
 						button:SetIcon(iconTexture, 18, 18, "overlay", {iconTexcoord.l, iconTexcoord.r, iconTexcoord.t, iconTexcoord.b}, nil, 2, 2)
@@ -177,7 +154,9 @@ function RA.OpenMainOptions (plugin)
 
 		--create the left menu scroll
 		local createMenuScrollBoxLine = function (parent, index)
-			local newButton = RA:CreateButton (parent, on_select_plugin, CONST_MENU_BUTTON_WIDTH, CONST_MENU_BUTTON_HEIGHT, "", 0, nil, nil, nil, nil, 1, button_template, button_text_template)
+			local newButton = RA:CreateButton (parent, onSelectPlugin, CONST_MENU_BUTTON_WIDTH, CONST_MENU_BUTTON_HEIGHT, "", 0, nil, nil, nil, nil, 1, button_template, button_text_template)
+			--newButton:SetTextSize(CONST_MENU_FONT_SIZE)
+			newButton.textsize = CONST_MENU_FONT_SIZE
 			newButton:SetPoint("topleft", parent, "topleft", 2, -1 + ((index - 1) * -CONST_MENU_BUTTON_HEIGHT))
 			return newButton
 		end
@@ -224,44 +203,7 @@ function RA.OpenMainOptions (plugin)
 	
 		--if a plugin requested to open its options
 		if (plugin) then
-			on_select_plugin (nil, nil, plugin)
-		end
-
-		--create a new button for a plugin in the left menu DEPRECATED
-		function f:CreatePluginButtonOnMenu (plugin, menuScrollBox)
-	
-			if (not plugin.IsDisabled) then
-				local icon_texture, icon_texcoord, text, text_color = plugin.menu_text (plugin)
-				local button = RA:CreateButton (menuScrollBox, on_select_plugin, f.Menu.button_width, f.Menu.button_height, text, plugin, nil, nil, nil, nil, 1, button_template, button_text_template)
-				
-				if (icon_texcoord) then
-					button:SetIcon (icon_texture, 18, 18, "overlay", {icon_texcoord.l, icon_texcoord.r, icon_texcoord.t, icon_texcoord.b}, nil, 2, 2)
-				else
-					button:SetIcon (icon_texture, 18, 18, "overlay", {0, 1, 0, 1}, nil, 2, 2)
-				end
-				
-				if (not plugin.IsBossMod) then
-					button:SetPoint (f.Menu.x, f.Menu.y)
-					f.Menu.y = f.Menu.y - (f.Menu.button_height+1)		
-				else
-					button:SetPoint (f.Mods.x, f.Mods.y)
-					f.Mods.y = f.Mods.y - (f.Mods.button_height+1)	
-				end
-				
-				local options_frame = CreateFrame ("frame", "RaidAssistOptionsPanel" .. (plugin.pluginname or math.random (1, 1000000)), f)
-				options_frame:Hide()
-				options_frame:SetSize (1, 1)
-				options_frame:SetPoint ("topleft", f, "topleft", f.Main.x, f.Main.y)
-				
-				plugin.OptionsPanel = options_frame
-				
-				f.AllOptionsPanels [plugin] = options_frame
-				f.AllOptionsButtons [plugin] = button
-				
-				if (plugin.IsDisabled) then
-					button:Disable()
-				end
-			end
+			onSelectPlugin (nil, nil, plugin)
 		end
 end
 
@@ -353,7 +295,7 @@ function RA.CreateHotkeyFrame(f)
 
 	local set_key_bind = function (self, button, keybindIndex)
 		if (keyBindListener.IsListening) then
-			key = mouseKeys [button] or button
+			local key = mouseKeys [button] or button
 			return registerKeybind (keyBindListener, key)
 		end
 
@@ -366,8 +308,7 @@ function RA.CreateHotkeyFrame(f)
 		enter_the_key:SetPoint ("bottom", self, "top")
 	end
 	
-
-	local setKeybindButton = RA:CreateButton (f, set_key_bind, f.Menu.button_width, 20, "", _, _, _, "SetKeybindButton", _, 0, RA:GetTemplate ("button", "OPTIONS_BUTTON_TEMPLATE"), RA:GetTemplate ("font", "OPTIONS_FONT_TEMPLATE"))
+	local setKeybindButton = RA:CreateButton (f, set_key_bind, CONST_MENU_BUTTON_WIDTH, CONST_MENU_BUTTON_HEIGHT, "", _, _, _, "SetKeybindButton", _, 0, RA:GetTemplate ("button", "OPTIONS_BUTTON_TEMPLATE"), RA:GetTemplate ("font", "OPTIONS_FONT_TEMPLATE"))
 	setKeybindButton:SetPoint ("bottomleft", f, "bottomleft", 10, 30)
 	setKeybindButton:SetClickFunction (set_key_bind, nil, nil, "left")
 	setKeybindButton:SetClickFunction (set_key_bind, nil, nil, "right")
@@ -376,16 +317,4 @@ function RA.CreateHotkeyFrame(f)
 	
 	local keybind_text = RA:CreateLabel (f, "Options Panel Keybind:")
 	keybind_text:SetPoint ("bottom", setKeybindButton, "top", 0, 2)
-	
-	local button_text_template = RA:GetTemplate ("font", "OPTIONS_FONT_TEMPLATE")
-	local options_dropdown_template = RA:GetTemplate ("dropdown", "OPTIONS_DROPDOWN_TEMPLATE")
-	
-	local button_template = RA:InstallTemplate ("button", "RAIDASSIST_OPTIONS_BUTTON_TEMPLATE", {
-		backdrop = {edgeFile = [[Interface\Buttons\WHITE8X8]], edgeSize = 1, bgFile = [[Interface\Tooltips\UI-Tooltip-Background]], tileSize = 64, tile = true},
-		backdropcolor = {1, 1, 1, .3},
-		backdropbordercolor = {0, 0, 0, 1},
-		onentercolor = {1, 1, 1, .4},
-		onenterbordercolor = {1, 1, 1, .6},
-	})
-
 end
